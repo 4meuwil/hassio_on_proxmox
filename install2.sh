446105142fa56cab9cde1cf4b58bf78ba8fa8ec7
Jake L <44265003+4meuwil@users.noreply.github.com>
2020-11-03 22:36:43 -0600

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -35,13 +35,13 @@ with open('/etc/pve/.vmlist') as vmlist:
     vmids = json.load(vmlist)
 if 'ids' not in vmids:
     print(100)
-elif '101' not in vmids:
+elif '101' not in (vmids['ids']):
     print(101)
-elif '102' not in vmids:
+elif '102' not in (vmids['ids']):
     print(102)
-elif '103' not in vmids:
+elif '103' not in (vmids['ids']):
     print(103)
-elif '104' not in vmids:
+elif '104' not in (vmids['ids']):
     print(104)
 else:
     last_vm = sorted(vmids['ids'].keys())[-1:][0]
